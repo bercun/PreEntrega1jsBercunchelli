32a2798e99b99803fb6ba02918c6a4ef935d092b
bercun <bercun@gmail.com>
2024-07-14 17:34:31 -0300

    pruebas de codicionales

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 // funcion para jugar al piedra papel o tijera
 
 // iniciar variables
-let computer = compuChois();
-let user = "";  
+
+
+let computer = compuChoise();
+let user = " ";  
 let attemp = 0;
 let win = 0;   
 let lose = 0;
 let draw = true;
 
+console.log(computer + " " + user + " " + attemp + " " + win + " " + lose + " " + draw    
+);
+
 // funcion para que la comoputadora elija una opcion
 
-function compuChois() {
+function compuChoise() {
     let computerChoice =["piedra", "papel", "tijera"];
     let random = Math.floor(Math.random() * 3);
     let computer = computerChoice[random];
@@ -19,20 +24,21 @@ function compuChois() {
 }
 // funcion para que el usuario elija una opcion
 function userChoise() {
-    let userChoice = prompt("Elige: piedra, papel o tijera").toLowerCase();
-    if (userChoice === "piedra" || userChoice === "papel" || userChoice === "tijera") {
-        return userChoice;
+    let user = prompt("Elige: piedra, papel o tijera").toLowerCase();
+    if (user === "piedra" || user === "papel" || user === "tijera") {
+        // alert("vos elegiste: " + user);
+        return user;
     } else {
         alert("Por favor elige una opcion valida");
         return userChoise();
     }
-    return userChoice;
+    return user;
 }
 
 // funcion para iniciar la comprobacion del juego
 function gameStart() {
     user = userChoise();
-    computer = compuChois();
+    computer = compuChoise();
     if (user === computer) {
         draw = true;
         alert("Empate");
@@ -41,29 +47,22 @@ function gameStart() {
             alert("Jugaste " + attemp + " veces, ganaste " + win + " veces y perdiste " + lose + " veces");
             return;
         }
-        gameStart();
     } else if (user === "piedra" && computer === "tijera" || user === "papel" && computer === "piedra" || user === "tijera" && computer === "papel") {
         win++;
         draw = false;
-        attempt++;
-        if (attempt === 3) {
+        attemp++;
+        if (attemp === 3) {
             alert("Jugaste " + attemp + " veces, ganaste " + win + " veces y perdiste " + lose + " veces");
+            alert("Ganaste"+ " La computadora eligio: " + computer);
             return;
         }
-        alert("Ganaste"+ " La computadora eligio: " + computer);
+        
     } else {
         lose++;
         draw = false;
-        attempt++;
+        attemp++;
         alert("Perdiste");
         gameStart();    
-    }
-    attemp++;
-    if (draw === true) {
-        gameStart();
-    } else {
-        alert("Jugaste " + attemp + " veces, ganaste " + win + " veces y perdiste " + lose + " veces");
-        gameStart();
     }
 }
 
