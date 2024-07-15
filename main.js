d2bd76922fc8843289cf2c6ad2ac6826f578b470
bercun <bercun@gmail.com>
2024-07-15 20:32:12 -0300

    mejorando la logica

--- main.js
+++ main.js
@@ -2,16 +2,11 @@
 
 // iniciar variables
 
-
-let computer = compuChoise();
-let user = " ";  
 let attemp = 0;
 let win = 0;   
 let lose = 0;
 let draw = true;
 
-console.log(computer + " " + user + " " + attemp + " " + win + " " + lose + " " + draw    
-);
 
 // funcion para que la comoputadora elija una opcion
 
@@ -19,50 +14,60 @@ function compuChoise() {
     let computerChoice =["piedra", "papel", "tijera"];
     let random = Math.floor(Math.random() * 3);
     let computer = computerChoice[random];
-    // alert("La computadora eligio: " + computer);
     return computer;
 }
 // funcion para que el usuario elija una opcion
 function userChoise() {
     let user = prompt("Elige: piedra, papel o tijera").toLowerCase();
     if (user === "piedra" || user === "papel" || user === "tijera") {
-        // alert("vos elegiste: " + user);
         return user;
     } else {
-        alert("Por favor elige una opcion valida");
+        console.log("Por favor elige una opcion valida");
         return userChoise();
     }
-    return user;
 }
+    
 
 // funcion para iniciar la comprobacion del juego
-function gameStart() {
-    user = userChoise();
-    computer = compuChoise();
+function gameRules(user, computer) {
     if (user === computer) {
-        draw = true;
-        alert("Empate");
-        attemp++;
-        if (attemp === 3) {
-            alert("Jugaste " + attemp + " veces, ganaste " + win + " veces y perdiste " + lose + " veces");
-            return;
-        }
-    } else if (user === "piedra" && computer === "tijera" || user === "papel" && computer === "piedra" || user === "tijera" && computer === "papel") {
-        win++;
-        draw = false;
-        attemp++;
-        if (attemp === 3) {
-            alert("Jugaste " + attemp + " veces, ganaste " + win + " veces y perdiste " + lose + " veces");
-            alert("Ganaste"+ " La computadora eligio: " + computer);
-            return;
-        }
+    return "Empate";
+    }
+    else if (user === "piedra" && computer === "tijera" || user === "papel" && computer === "piedra" || user === "tijera" && computer === "papel") {
+        return "Ganaste";
+    }
         
-    } else {
-        lose++;
-        draw = false;
+    else {
+        return "Perdiste";
+    }     
+    
+}
+function gameStart() {
+    while (attemp < 3){
+        let computer = compuChoise();
+        let user = userChoise();
+        let result = gameRules(user, computer);
+        console.log("La computadora eligio: " + computer);
+        console.log("Tu elegiste: " + user);
+        console.log("El resultado es: " + result);
+        if (result === "Ganaste") {
+            win++;
+        } else if (result === "Perdiste") {
+            lose++;
+        } else {
+            draw = false;
+        }
         attemp++;
-        alert("Perdiste");
-        gameStart();    
+        console.log("Intentos: " + attemp);
+        console.log("Ganados: " + win);
+        console.log("Perdidos: " + lose);
+        console.log("Empates: " + draw);
+        let playAgain = confirm("Quieres jugar de nuevo?");
+        if (playAgain) {
+            gameStart();
+        } else {
+        console.log("Gracias por jugar");
+    }
     }
 }
 
